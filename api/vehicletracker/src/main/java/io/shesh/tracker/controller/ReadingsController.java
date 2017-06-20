1cc6c7714d892c74eb548537425f21be4b476b96
vshes <vshes.60@gmail.com>
2017-06-20 17:32:41 -0500

    (chors) Swagger Configured

--- api/vehicletracker/src/main/java/io/shesh/tracker/controller/ReadingsController.java
+++ api/vehicletracker/src/main/java/io/shesh/tracker/controller/ReadingsController.java
@@ -4,6 +4,8 @@ import io.shesh.tracker.model.Readings;
 import io.shesh.tracker.model.Alert;
 import io.shesh.tracker.service.AlertService;
 import io.shesh.tracker.service.ReadingsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +17,7 @@ import java.util.List;
  */
 @RestController
 @CrossOrigin(origins = {"http://localhost:63342","http://localhost:3000","http://mocker.egen.io"},maxAge = 3600)
-
+@Api(tags = "Readings",description = "Readings End Point")
 public class ReadingsController {
 
     @Autowired
@@ -25,11 +27,13 @@ public class ReadingsController {
     private AlertService alertService;
 
     @RequestMapping(method = RequestMethod.GET,value = "readings")
+    @ApiOperation(value = "Find all Readings",notes = "Displays all the Readings received for all Vehicles")
     public List<Readings> findAllReadings(){
         return readingsService.findAll();
     }
 
     @RequestMapping(method = RequestMethod.GET,value = "readings/{vid}")
+    @ApiOperation(value = "Find readings for particular VIN",notes = "Displays all the Readings received for for a Vehicle identified by VIN")
     public List<Readings> findReadingsByVehicleId(@PathVariable("vid") String id){
         System.out.println("ID IS"+id);
         return readingsService.findReadingByVehicleId(id);
@@ -39,6 +43,8 @@ public class ReadingsController {
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE
     )
+    @ApiOperation(value = "Return the Alerts for Reading Received",notes = "Displays all the Alerts generated for the Reading")
+
     public List<Alert> create(@RequestBody Readings readings){
         readingsService.create(readings);
         return alertService.createAlertsForReading(readings);
@@ -48,11 +54,14 @@ public class ReadingsController {
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE
     )
+    @ApiOperation(value = "Update Readings",notes = "Update the readings")
     public Readings update(@RequestBody Readings readings){
         return   readingsService.update(readings);
     }
 
     @RequestMapping(value = "reading/{vid}",method = RequestMethod.DELETE)
+    @ApiOperation(value = "Delete all Readings for a Vehicle",notes = "Deletes all the Readings received for a Vehicle identified by VIN")
+
     public void update(@PathVariable("vid") String  id){
         readingsService.delete(id);
     }
--- api/vehicletracker/src/main/java/io/shesh/tracker/controller/VehicleController.java
+++ api/vehicletracker/src/main/java/io/shesh/tracker/controller/VehicleController.java
@@ -3,6 +3,8 @@ package io.shesh.tracker.controller;
 import io.shesh.tracker.exception.ResourceNotFoundException;
 import io.shesh.tracker.model.Vehicle;
 import io.shesh.tracker.service.VehicleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -13,21 +15,26 @@ import java.util.List;
  */
 @RestController
 @CrossOrigin(origins = {"http://localhost:63342","http://localhost:3000","http://mocker.egen.io"},maxAge = 3600)
+@Api(value = "Vehicles", tags = "Vehicles",description = "Vehicles End Point")
 public class VehicleController {
 
     @Autowired
     VehicleService vehicleService;
 
     @RequestMapping(method = RequestMethod.GET,value = "vehicles")
+    @ApiOperation(value = "Find all Vehicles",notes = "Displays all the Vehicles in the DB")
+
     List<Vehicle>  findAllVehicle(){
         return vehicleService.findAll();
     }
 
     @RequestMapping(method = RequestMethod.GET,value = "vehicle/{id}")
+    @ApiOperation(value = "Find Vehicle By Id",notes = "Displays the Vehicle for the ID provided from the DB")
+
     Vehicle  findVehicleById(@PathVariable("id") String id){
        Vehicle vehicle =vehicleService.findById(id);
        if(vehicle == null){
-           throw new ResourceNotFoundException("Vehicle Not Fpund");
+           throw new ResourceNotFoundException("Vehicle Not Found");
        }
        else{
            return vehicle;
@@ -39,6 +46,8 @@ public class VehicleController {
         consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
         produces = MediaType.APPLICATION_JSON_UTF8_VALUE
     )
+    @ApiOperation(value = "Create new Vehicle",notes = "Displays the Vehicle created ")
+
     Vehicle create(@RequestBody Vehicle vehicle){
         return vehicleService.create(vehicle);
     }
@@ -47,6 +56,8 @@ public class VehicleController {
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE
     )
+    @ApiOperation(value = "Update the Vehicle",notes = "Displays the Vehicle Updated ")
+
     Vehicle[] create(@RequestBody Vehicle[] vehicle){
         return vehicleService.create(vehicle);
     }
@@ -61,6 +72,8 @@ public class VehicleController {
     }
 
     @RequestMapping(value = "vehicle/{id}",method = RequestMethod.DELETE)
+    @ApiOperation(value = "Delete Vehicle",notes = "Removes the Vehicle identified by VIN ")
+
     void update(@PathVariable("id") String  id){
         vehicleService.delete(id);
     }
